a87b049704498edb943d991e44db9c345aaec91e
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-10-15 17:19:15 +0530

    Solve exercises on page 90

---
+++ ch2/p90.hs
@@ -0,0 +1,15 @@
+-- 1.
+e1 = x * 3 + y
+ where x = 3
+       y = 1000
+
+-- 2.
+e2 = x * 5
+ where y = 10
+       x = 10 * 5 + y
+
+-- 3.
+e3 = z / x + y
+ where x = 7
+       y = negate x
+       z = y * 10
