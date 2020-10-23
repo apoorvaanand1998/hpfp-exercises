27de1587eb03425051a219be18a98b4a0d2e8d36
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-10-23 17:58:58 +0530

    Solve third exercise of Eq Instances

---
+++ ch6/eq_instances_3.hs
@@ -0,0 +1,6 @@
+data StringOrInt = TisAnInt Int | TisAString String
+
+instance Eq StringOrInt where
+  (==) (TisAnInt x) (TisAnInt y) = x == y
+  (==) (TisAString x) (TisAString y) = x == y
+  (==) _ _ = False
