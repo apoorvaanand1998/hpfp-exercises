1f3970e8ec0d186d821b1c42896547d2b4d85b81
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-11-04 15:07:57 +0530

    Finish Vehicles exercise

---
+++ ch11/p420m.hs
@@ -0,0 +1,44 @@
+data Price = Price Integer deriving (Eq, Show)
+
+data Manufacturer = Mini | Mazda | Tata deriving (Eq, Show)
+
+data Airline =
+  PapuAir
+  | CatapultsR'Us
+  | TakeYourChancesUnited
+  deriving (Eq, Show)
+
+data Size = Size Integer deriving (Eq, Show) -- added
+
+data Vehicle = Car Manufacturer Price
+             | Plane Airline Size -- modified
+             deriving (Eq, Show)
+
+myCar = Car Mini (Price 14000)
+urCar = Car Mazda (Price 20000)
+clownCar = Car Tata (Price 7000)
+doge = Plane PapuAir (Size 420) -- modified
+
+-- 1.
+-- Vehicle
+
+-- 2.
+
+isCar :: Vehicle -> Bool
+isCar (Car _ _) = True
+isCar _ = False
+
+isPlane :: Vehicle -> Bool
+isPlane (Plane _ _) = True
+isPlane _ = False
+
+areCars :: [Vehicle] -> [Bool]
+areCars = map isCar
+
+-- 3.
+
+getManu :: Vehicle -> Manufacturer
+getManu (Car m _) = m
+
+-- 4.
+-- If used with Plane, non-exhaustive pattern match
