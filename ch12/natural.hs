a560f6790964d7b53f66d62e3a963aa00ac65cc4
Apoorva Anand <apoorvaanand.work@gmail.com>
2020-11-09 17:35:45 +0530

    Finish It's only Natural exercise

---
+++ ch12/natural.hs
@@ -0,0 +1,12 @@
+data Nat = Zero | Succ Nat deriving (Eq, Show)
+
+natToInteger :: Nat -> Integer
+natToInteger Zero = 0
+natToInteger (Succ x) = 1 + natToInteger x
+
+integerToNat :: Integer -> Maybe Nat
+integerToNat x
+  | x < 0 = Nothing
+  | otherwise = Just (f x)
+  where f 0 = Zero
+        f x = Succ (f (x-1))
